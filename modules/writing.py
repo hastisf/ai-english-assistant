d000c168062aba212d778d6db38c622aa9869be3
hastisf <hastisfatmawati@gmail.com>
2026-08-05 09:25:19 +0700

    Update writing.py

---
+++ modules/writing.py
@@ -0,0 +1,13 @@
+from modules.prompts import WRITING_PROMPT
+
+
+def evaluate_writing(
+    text: str,
+    cefr_level: str,
+    writing_type: str
+):
+    """
+    Evaluate user writing using Gemini.
+    """
+
+    pass
