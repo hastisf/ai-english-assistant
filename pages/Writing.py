7eb30a1e51b12f157fc4333a03d744c0cea89eae
hastisf <hastisfatmawati@gmail.com>
2026-08-07 22:18:15 +0700

    Update Writing.py

--- pages/Writing.py
+++ pages/Writing.py
@@ -7,7 +7,7 @@ from modules.writing import evaluate_writing
 
 st.markdown(
 """
-<h2 style='font-size: clamp(1.4rem, 4vw, 2.2rem); margin-bottom:0.5rem; font-weight:700; color:#0F172A;'>
+<h2 style='font-size: clamp(1.4rem, 4vw, 2.2rem); margin-bottom:0.2rem; font-weight:700; color:#0F172A;'>
     Writing Evaluation
 </h2>
     
